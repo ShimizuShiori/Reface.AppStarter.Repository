0a57eff5d62feb6b34374a608cd75c8b89610460
ShimizuShiori <116971263@qq.com>
2020-04-12 19:44:11 +0800

    定义仓库的接口类型

---
+++ src/Reface.AppStarter.Repository/Reface.AppStarter.Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Reface.AppStarter.Repository
+{
+    public interface IRepository<TEntity>
+        where TEntity : class, new()
+    {
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        void Clear();
+        ISelectResult<TEntity> Select();
+    }
+}
---
+++ src/Reface.AppStarter.Repository/Reface.AppStarter.Repository/ISelectResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Reface.AppStarter.Repository
+{
+    public interface ISelectResult<TEntity>
+            where TEntity : class
+    {
+        ISelectResult<TEntity> Where(Expression<Func<TEntity, bool>> expression);
+        ISelectResult<TEntity> OrderByAsc(Expression<Func<TEntity, object>> expression);
+        ISelectResult<TEntity> OrderByDesc(Expression<Func<TEntity, object>> expression);
+        IEnumerable<TEntity> ToList();
+
+        IEnumerable<TEntity> ToPageList(int pageIndex, int pageSize);
+
+        IEnumerable<TEntity> ToPageList(int pageIndex, int pageSize, out int count);
+        TEntity First();
+        TEntity FirstOrNull();
+        int Count();
+    }
+}
